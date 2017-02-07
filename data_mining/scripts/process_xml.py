457ce9f17d437602ce0771f4495cf0ca56c1f8b6
Jamie Adkins <jamieadkins95@gmail.com>
2017-02-07 15:24:46 +0000

    Try to get images from gwentify
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- data_mining/scripts/process_xml.py
+++ data_mining/scripts/process_xml.py
@@ -5,6 +5,8 @@ import os
 import json
 import re
 
+from unidecode import unidecode
+
 def saveJson(filename, cardList):
     filepath = os.path.join("../outputs/" + filename)
     print("Saving %s cards to: %s" % (len(cardList), filepath))
@@ -262,6 +264,33 @@ def evaluateTokens(cards):
                     tokenId = template.attrib['V']
                     if cards.get(tokenId) != None:
                         cards.get(tokenId)['released'] = True
+                        
+NAME_REPLACE = {" ": "_", ":": "", "'": "", "`": "", "’": "", "(": "", ")": "", "-": "_"}
+                        
+# Transform the given name to an url friendly format.
+def getNameKey(name):
+    # https://stackoverflow.com/questions/6116978/python-replace-multiple-strings
+    name = name.lower()
+
+    # In one pass, replace a few space characters and special characters.
+    rep = dict((re.escape(k), v) for k, v in NAME_REPLACE.items())
+    pattern = re.compile("|".join(rep.keys()))
+    name = pattern.sub(lambda m: rep[re.escape(m.group(0))], name)
+
+    # Tries to represent the Unicode data in ASCII characters. It will remove accents and the likes.
+    name = unidecode(name)
+    return name
+
+def getImageUrls(cards):
+    with open('../../output/latest.json') as data_file:    
+        data = json.load(data_file)
+        for gwentify in data:
+            for cardId in cards:
+                card = cards[cardId]
+                print(getNameKey(card['name']))
+                if getNameKey(card['name']) == gwentify['key']:
+                    for variationId in card['variations']:
+                        card['variations'][variationId]['art']['fullsizeImageUrl'] = gwentify['variations'][0]['art']['fullsizeImageUrl']
 
 
 xml_folder = sys.argv[1]
@@ -303,6 +332,8 @@ cardData = createCardJson()
 evaluateInfoData(cardData)
 # We have to do this as well to catch cards like Botchling, that are explicitly named in the Baron's tooltip.
 evaluateTokens(cardData)
+
+getImageUrls(cardData)
 
 saveJson("latest.json", cardData)
 
