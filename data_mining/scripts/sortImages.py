b6eead9613e6bad50feba7eeb7d796d5ad563745
Jamie Adkins <jamieadkins95@gmail.com>
2017-02-15 17:24:03 +0000

    Can now pass image directory as argument to sortImages
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- data_mining/scripts/sortImages.py
+++ data_mining/scripts/sortImages.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 
 import os
+import sys
 
-inputDirectory = "./"
+def printUsage():
+    print("Please supply the directory of the images as an argument when running this script.")
+    print("e.g.")
+    print("./sortImages.py ../images")
+
+if len(sys.argv) > 1:
+    inputDirectory = sys.argv[1]
+else:
+    printUsage()
+    exit()
+
+# Add a backslash on the end if it doesn't exist.
+if inputDirectory[-1] != "/":
+    inputDirectory = inputDirectory + "/"
+
+if not os.path.isdir(inputDirectory):
+    print(inputDirectory + " is not a valid directory")
+    printUsage()
+    exit()
+
 output = "../images"
 
 if not os.path.exists(output):
